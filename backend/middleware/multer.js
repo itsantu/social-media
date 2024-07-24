7362f919d4b01d827d2482cae73ec4c657c2535c
itsantu <mallickantu19@gmail.com>
2024-07-24 17:18:57 +0530

    public folder modified

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,9 +1,6 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp")
-  },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now());
   },
