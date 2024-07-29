d92a25d46ff0358b3ac4752de798879d0679056e
Swaraj-Halder <154491830+Swaraj-Halder@users.noreply.github.com>
2024-07-29 21:29:18 +0530

    Update postModel.js
    added image expire time

--- backend/models/postModel.js
+++ backend/models/postModel.js
@@ -13,17 +13,21 @@ const postSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    // the reaon of adding this is about in post controller
+    imageExpire:{
+        type: String,
+        required: true
+    },
     createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true,
     },
-    createdAt:{
-        type:Date,
-        default:(Date.now())*1000,
-        expires:'604800s'
-    }
-}, {timestamps: true})
+},
+{
+    timestamps: true,
+    expire:"604800s"
+})
 
 const Post = mongoose.model("post", postSchema)
 
