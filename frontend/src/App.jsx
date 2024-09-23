9146537cbb38e5799f7beaa737bc1771c34ebb9b
Antu Mallick <123813461+itsantu@users.noreply.github.com>
2024-09-24 01:37:42 +0530

    router issue fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useAuthContext} from '../hooks/useAuthContext'
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // pages and components
 import Navbar from "./components/Navbar";
@@ -11,23 +11,57 @@ import Signup from "./pages/Signup";
 import About from "./pages/About";
 import ChangePassword from "./components/ChangePassword";
 import DeleteUser from "./components/DeleteUser";
+import ForgotPasswordEmail from "./components/ForgotPasswordEmail";
+import ForgotPWPage from "./components/ForgotPWPage";
 
 function App() {
-  const {user} = useAuthContext()
+  const { user } = useAuthContext();
 
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={user ? <Home /> : <Navigate to="/signup"/> }/>
-          <Route path="/upload" element={user ? <PostForm /> : <Navigate to="/signup"/>}/>
-          <Route path="/update" element={user ? <EditForm /> : <Navigate to="/signup"/>} />
-          <Route path="/about" element={user ? <About /> : <Navigate to="/signup"/>}/>
-          <Route path="about/changepassword" element={user ? <ChangePassword /> : <Navigate to="/signup"/>} />
-          <Route path="about/deleteuser" element={user ? <DeleteUser /> : <Navigate to="/signup"/>} />
-          <Route path="/login" element={!user ? <Login /> : <Navigate to="/"/>}/>
-          <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/"/>}/>
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="/upload"
+            element={user ? <PostForm /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="/update"
+            element={user ? <EditForm /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="/about"
+            element={user ? <About /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="about/changepassword"
+            element={user ? <ChangePassword /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="about/deleteuser"
+            element={user ? <DeleteUser /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login/forgot-password-email"
+            element={!user ? <ForgotPasswordEmail /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/login/forgot-password"
+            element={!user ? <ForgotPWPage /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/signup"
+            element={!user ? <Signup /> : <Navigate to="/" />}
+          />
         </Routes>
       </BrowserRouter>
     </>
