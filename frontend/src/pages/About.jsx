3480c28ea8fb640a7824f9e3c78ebb3a0da47114
Antu Mallick <123813461+itsantu@users.noreply.github.com>
2024-09-27 01:19:22 +0530

    nav ui

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
-import { IoEye, IoEyeOffSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { IoLogOutOutline } from "react-icons/io5";
 import { useLogout } from "../../hooks/useLogout";
@@ -9,30 +7,12 @@ useLogout
 const About = () => {
   const { user } = useAuthContext();
   const {logout} = useLogout();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [changePassword, setChangePassword] = useState(false);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    console.log(email, password);
-  };
-
-  const handleLogOut = () => {
-    logout()
-  };
-
-  const handleShowPassword = () => {
-    setShowPassword(!showPassword);
-  };
 
   return (
     <div className="container  mx-auto px-10 py-10 rounded-3xl shadow-2xl w-screen md:max-w-[600px] mt-24">
       <div className="flex justify-between items-center mb-5">
         <h2 className="text-3xl font-semibold ">@{user.uname}</h2>
-        <p onClick={handleLogOut} className="flex items-center gap-2 text-sm md:text-md cursor-pointer px-3 py-2 bg-slate-200 hover:bg-slate-400 rounded-md duration-200">Logout <IoLogOutOutline className="text-gray-950"/></p>
+        <p onClick={() => logout()} className="flex items-center gap-2 text-sm md:text-md cursor-pointer px-3 py-2 bg-slate-200 hover:bg-slate-400 rounded-md duration-200">Logout <IoLogOutOutline className="text-gray-950"/></p>
       </div>
       <h3 className="font-bold mb-4">Email: </h3>
       <input
